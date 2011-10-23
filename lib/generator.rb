62301bec1425a20c079311f82b515bed3d63bffe
Stefan Wienert <stwienert@gmail.com>
2011-10-23 14:34:03 +0200

    pygmentize generator for syntaxhl

--- lib/generator.rb
+++ lib/generator.rb
@@ -5,18 +5,17 @@ module DeckJSBuilder
   MAIN_DIR = File.join File.dirname(__FILE__), ".."
 
   include PresentationHelper
-
   META = YAML.load_file File.join(MAIN_DIR, "meta.yml")
 
   def render(path,b=binding)
     path = File.join MAIN_DIR, path
-    Haml::Engine.new(IO.read(path)).render(b)
+    Haml::Engine.new(IO.read(path), :ugly => true).render(b)
   end
   def build(from_file, to_file)
     text = render from_file, binding
     write_to = File.join MAIN_DIR, to_file
     File.open(write_to,"w+") {|f| f.write text}
-    puts "Written #{write_to}"
+    puts "[#{Time.now.strftime("%H:%M:%S")}] written #{write_to}"
   end
 
   def css
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,4 +3,26 @@ module PresentationHelper
   def image_tag_with_caption(img_path, caption)
     "<div class='image-wrap'><img src='#{img_path}'/><div class='caption'>#{caption}</div>"
   end
+  def pygmentize(lexer,&block)
+    text = capture_haml do
+      yield
+    end
+    require "digest"
+    filename = ".pygments-cache/" + Digest::MD5.hexdigest(text) + ".txt"
+    if File.exists? filename
+      puts "loading syntax hl from cache"
+      return IO.read(filename)
+    end
+
+    pygmentize = IO.popen("pygmentize -f html -l #{lexer}", "w+")
+    pygmentize.puts text
+    pygmentize.close_write
+    result = pygmentize.gets(nil)
+    pygmentize.close
+
+    FileUtils.mkdir_p(".pygments-cache")
+    File.open(filename,"w+") {|f| f.write(result)}
+    result
+  end
+
 end
